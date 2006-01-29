0abc74ed14ff072d5879b9e053e44105964e9be6
mark <mark>
2006-01-29 07:00:04 +0000

    *** empty log message ***

--- Makefile
+++ Makefile
@@ -2,4 +2,6 @@ CFLAGS = $(WARNINGS) $(DEFINES) $(INCLUDES)
 DEFINES = -D_XOPEN_SOURCE=600
 INCLUDES = -I${LUA}/src
 WARNINGS = -W -Wall
-LUA = /home/mark/src/lang/lua/lua-5.0.2
+LUA = /home/mark/src/lang/lua/lua-5.1-rc1
+
+lu.so: lu.o; $(CC) -shared -o $@ lu.o
--- tests/rt1.lua
+++ tests/rt1.lua
@@ -4,10 +4,11 @@ require "ex"
 --print"os.sleep"
 --os.sleep(2);
 
-print"os.setenv"
 assert(os.setenv("foo", "42"))
+print("expect foo= 42")
 print("foo=", os.getenv("foo"))
 assert(os.unsetenv("foo"))
+print("expect foo= nil")
 print("foo=", os.getenv("foo"))
 
 
